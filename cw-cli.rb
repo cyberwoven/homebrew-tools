e97fb1085c912b3712fd799357df16b0af1a4d43
Drew King <drew@drewcking.com>
2022-11-23 16:30:25 -0500

    v1.2.6

--- cw-cli.rb
+++ cw-cli.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CwCli < Formula
   desc "Command line tool for local web application development"
   homepage "https://github.com/cyberwoven/cw-cli"
-  url "https://github.com/cyberwoven/cw-cli/archive/refs/tags/v1.2.5.tar.gz"
-  sha256 "a7f376365d6916bd57cfc46c2db74a37d025ad4397372052a5fad3bf7c7091e4"
+  url "https://github.com/cyberwoven/cw-cli/archive/refs/tags/v1.2.6.tar.gz"
+  sha256 "bff27446f13b8dc7b15dd1fed30318055f94c79d9501c24244375adcf9f475a7"
   license "MIT"
 
   depends_on "mydumper"
@@ -15,15 +12,6 @@ class CwCli < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cw-cli`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/cw --help"
   end
 end
