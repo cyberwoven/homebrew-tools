0c211d8c79e75997e94a83e3b9ccf2716ea809fa
Drew King <drew@drewcking.com>
2022-12-20 12:17:17 -0500

    cw-cli: change mydumper dependency to CW formula

--- cw-cli.rb
+++ cw-cli.rb
@@ -5,7 +5,7 @@ class CwCli < Formula
   sha256 "bff27446f13b8dc7b15dd1fed30318055f94c79d9501c24244375adcf9f475a7"
   license "MIT"
 
-  depends_on "mydumper"
+  depends_on "cyberwoven/tools/mydumper"
 
   def install
     bin.install "bin/cw"
