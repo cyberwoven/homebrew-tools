6eb3cf03d9e8fd82a9e15085746facaa15ecfa95
Drew King <drew@drewcking.com>
2023-07-25 17:49:58 -0400

    v2.3.9

--- cw-cli.rb
+++ cw-cli.rb
@@ -1,8 +1,8 @@
 class CwCli < Formula
   desc "Command line tool for local web application development"
   homepage "https://github.com/cyberwoven/cw-cli"
-  url "https://github.com/cyberwoven/cw-cli/archive/refs/tags/v2.3.8.tar.gz"
-  sha256 "da07646ca2c6eabfc873d004a3327538e31c126cb5e23adb56213ef5fd6ce812"
+  url "https://github.com/cyberwoven/cw-cli/archive/refs/tags/v2.3.9.tar.gz"
+  sha256 "f9107fa7f05ca13d30a710a4e3097d5c153830458debbc3813f9589cc7587d7b"
   license "MIT"
 
   depends_on "cyberwoven/tools/mydumper"
